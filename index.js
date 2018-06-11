f6b844155885a3a66fbf4a91e35ee9bc4f0789c3
Chris Sergienko <chris@praekelt.com>
2018-06-11 11:56:13 +0200

    Use __dirname for finding yaml file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yaml = require('js-yaml'),
     fs = require('fs');
 
-const _CONFIG = yaml.safeLoad(fs.readFileSync('./.eslintrc.yml', 'utf-8'));
+const _CONFIG = yaml.safeLoad(fs.readFileSync(`${__dirname}/.eslintrc.yml`, 'utf-8'));
 
 module.exports = _CONFIG;
